2a2c4da0ad889d336f66619df2617f2a17ad2125
malayvasa <malayvasa@gmail.com>
2020-11-13 01:14:12 +0530

    twitter embed commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let CNN =0;
 let REP =0;
 let NDTV =0;
+
+
 function addCNN() {
     CNN++;
     console.log("CNN:"+CNN);
@@ -18,14 +20,17 @@ function result() {
     if (CNN>NDTV && CNN>REP){
           document.getElementById("result-cnn").style.display = "block";
           document.getElementById("result-cnn").classList.toggle('card-img-top');
+          document.getElementById("twitter-cnn").style.display ="block";
     }
     else if (NDTV>CNN && NDTV>REP){
         document.getElementById("result-ndtv").style.display = "block";
         document.getElementById("result-ndtv").classList.toggle('card-img-top');
+        document.getElementById("twitter-ndtv").style.display ="block";
     }
     else if (REP>CNN && NDTV<REP){
         document.getElementById("result-rep").style.display = "block";
         document.getElementById("result-rep").classList.toggle('card-img-top');
+        document.getElementById("twitter-republic").style.display ="block";
     }
     else if(NDTV==0 && CNN==0 && REP==0){
         alert("Invalid Selection");
